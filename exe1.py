5c2623d8f929547b1bbb3a346659defbeb87b79f
joaohenriquedo <joaohenriquedomingos2008@gmail.com>
2025-02-18 14:43:44 -0300

    alt

--- exe1.py
+++ exe1.py
@@ -1,2 +1,17 @@
-quantidade = input("insira o grau da aceitação de investimento")
-investimento = float(input("insira o valor do investimento" ))
+risco = input("Informe o grau de aceitação de risco (BX para baixo ou AL para alto): ")
+valor = int (input("Informe o valor a ser investido: "))
+if risco not in ["BX", "AL"]:
+        print("Dados inválidos para o grau de aceitação de risco.")
+           
+if risco == "BX":
+                if valor < 1000:
+                    print("Aplicação recomendada: Poupança")
+                else:
+                    print("Aplicação recomendada: Renda Fixa")
+elif risco == "AL":
+                if valor < 1000:
+                    print("Aplicação recomendada: Bitcoins")
+                else:
+                    print("Aplicação recomendada: Ações")
+else:
+            print("Dados inválidos para o valor. Informe um número real.")
---
+++ exe4.py
@@ -0,0 +1,15 @@
+sexo = input("informe seu sexo (homem ou mulher): ").lower()
+idade = int(input("informe sua idade: "))
+
+if sexo == "mulher":
+    if 21 <= idade <= 34:
+        print("você pode se alistar.")
+    else:
+        print("você não atende à faixa etária para o alistamento.")
+elif sexo == "homem":
+    if 18 <= idade <= 39:
+        print("você pode se alistar.")
+    else:
+        print("você não atende à faixa etária para o alistamento.")
+else:
+    print("sexo inválido. Por favor, informe 'homem' ou 'mulher'.")
