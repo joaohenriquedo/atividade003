88486a7fac2ac36715915b660d8ffba9193803a7
joaohenriquedo <joaohenriquedomingos2008@gmail.com>
2025-02-18 19:51:01 -0300

    Update exe6.py

--- exe6.py
+++ exe6.py
@@ -3,7 +3,7 @@ eleitores = int(input("Informe a quantidade de eleitores: "))
 votos = int(input("Informe a quantidade de votos do candidato mais votado: "))
 if eleitores >= 200000:
     percentual_votos = (votos /eleitores) * 100
-    if percentual_votos > 50:
+    if percentual_votos > 0.50:
         print("no município de {}, não haverá segundo turno.".format(municipio))
     else:
         print("no município de {}, haverá segundo turno.".format(municipio))
